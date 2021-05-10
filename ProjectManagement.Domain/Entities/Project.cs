cf93d6f02cc966f437298f15745e9c5be7dad0a2
Thomas Karlsson <thomas.karlsson@gmx.com>
2021-05-10 12:13:10 +0200

    Added domain project with entities.

---
+++ ProjectManagement.Domain/Entities/Project.cs
@@ -0,0 +1,18 @@
+﻿using ProjectManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Domain.Entities
+{
+    public class Project : AuditableEntity
+    {
+        public Guid ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public Guid TaskId { get; set; }
+        public ICollection<Task> Tasks { get; set; }
+    }
+}
---
+++ ProjectManagement.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using ProjectManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Domain.Entities
+{
+    public class User : AuditableEntity
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid ProjectId { get; set; }
+        public ICollection<Project> Projects { get; set; }
+    }
+}
